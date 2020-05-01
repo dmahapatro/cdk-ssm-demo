e47698762f87a66a5e3a526919fae9227b77a807
Dhiraj Mahapatro <mahadhir@amazon.com>
2020-05-01 16:46:33 -0400

    Added basic tests

--- test/ssm-demo.test.ts
+++ test/ssm-demo.test.ts
@@ -1,21 +1,56 @@
-import { expect as expectCDK, haveResource } from '@aws-cdk/assert';
+import { expect as expectCDK, haveResource, countResources } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as SsmDemo from '../lib/ssm-demo-stack';
 
-test('SQS Queue Created', () => {
+const environment = { 
+  env: { 
+    region: 'us-east-2', 
+    account: '1234' 
+  }
+};
+
+test('EC2 Instance Created', () => {
     const app = new cdk.App();
     // WHEN
-    const stack = new SsmDemo.SsmDemoStack(app, 'MyTestStack');
+    const stack = new SsmDemo.SsmDemoStack(app, 'MyTestStack', environment);
     // THEN
-    expectCDK(stack).to(haveResource("AWS::SQS::Queue",{
-      VisibilityTimeout: 300
-    }));
+    expectCDK(stack).to(haveResource("AWS::AutoScaling::AutoScalingGroup"));
 });
 
 test('SNS Topic Created', () => {
   const app = new cdk.App();
   // WHEN
-  const stack = new SsmDemo.SsmDemoStack(app, 'MyTestStack');
+  const stack = new SsmDemo.SsmDemoStack(app, 'MyTestStack', environment);
   // THEN
   expectCDK(stack).to(haveResource("AWS::SNS::Topic"));
+});
+
+test('SSM Assume Role Created', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new SsmDemo.SsmDemoStack(app, 'MyTestStack', environment);
+
+  // THEN
+  expectCDK(stack).to(haveResource("AWS::IAM::Role", {
+    "AssumeRolePolicyDocument": {
+      "Statement": [
+        {
+          "Action": "sts:AssumeRole",
+          "Effect": "Allow",
+          "Principal": {
+            "Service": [
+              "ssm.amazonaws.com",
+              "ec2.amazonaws.com"
+            ]
+          }
+        }
+      ],
+      "Version": "2012-10-17"
+    },
+    "ManagedPolicyArns": [
+      "arn:aws:iam::aws:policy/service-role/AmazonSSMAutomationRole"
+    ]
+  }));
+  
+  expectCDK(stack).to(countResources("AWS::IAM::Policy", 1));
 });
